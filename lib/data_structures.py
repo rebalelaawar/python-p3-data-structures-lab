08861b496ba718777424c1b1511b57a79be5e49c
rebalelaawar <rebalelaawar@gmail.com>
2023-11-13 15:31:25 -0500

    Data structures lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,45 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = [food['name']for food in spicy_foods]
+    return names
+
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spicy = [food for food in spicy_foods if food.get('heat_level', 0)>5]
+    return spicy
+   
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food.get('name')
+        cuisine = food.get('cuisine')
+        heat_level = food.get('heat_level')
+
+        output_string = f"{name} ({cuisine}) | Heat Level: {'🌶' *  heat_level}"
+        print(output_string)
+
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
+ 
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    heat_level = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(heat_level)
+
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    return sum(food['heat_level']for food in spicy_foods)/len(spicy_foods)
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
